a56f89d6e015fff0ad9e21683d9bf5b3b0b59ea0
baArlene <carloschinyuku@gmail.com>
2024-01-24 21:28:20 +0200

    "commit"

--- react-admin-dashboard/src/components/navbar/Navbar.tsx
+++ react-admin-dashboard/src/components/navbar/Navbar.tsx
@@ -3,7 +3,26 @@ import './navbar.scss'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>CarterDev</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className='icon' />
+        <img src="/app.svg" alt="" className='icon' />
+        <img src="/expand.svg" alt="" className='icon' />
+        <div className="notifications">
+          <img src="/notification.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://randomuser.me/api/portraits/lego/6.jpg" alt="" />
+          <span>Axyl</span>
+        </div>
+        <img src="/settings.svg" alt="" className='icon' />
+      </div>
+    </div>
   )
 }
 
