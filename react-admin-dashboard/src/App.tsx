4c2e5e0c9cc1c21b8fe71b77520b5d2585683b1d
baArlene <carloschinyuku@gmail.com>
2024-01-24 21:14:23 +0200

    +++

--- react-admin-dashboard/src/App.tsx
+++ react-admin-dashboard/src/App.tsx
@@ -9,6 +9,8 @@ import Products from './pages/products/Products';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Menu from './components/menu/Menu';
+import Login from './pages/login/Login';
+import './styles/global.scss'
 
 function App() {
 
@@ -47,7 +49,11 @@ function App() {
           element: (<Products />)
         },
       ]
-    }, 
+    },
+    {
+      path: '/login',
+      element:(<Login />)
+    } 
   ]);
 
 
